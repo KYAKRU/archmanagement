455e799517e92bd22414ddcaf1ced4032f1bcc8e
KYAKRU <115971301+KYAKRU@users.noreply.github.com>
2023-08-01 10:08:20 +0530

    Update eval.py

--- MukeshRobot/modules/eval.py
+++ MukeshRobot/modules/eval.py
@@ -135,7 +135,7 @@ __mod_name__ = "Eᴠᴀʟ"
 __help__ = """
 ★ᴏᴡɴᴇʀ ᴄᴍᴅ ★
 
-©️ ʙʏ ᯾ [ɴᴏᴏʙ Mᴜᴋᴇsʜ](f"tg://user?id={OWNER_ID}"))
+©️ ʙʏ ᯾ [ᴀʀᴄʜ ɴᴇᴛᴡᴏʀᴋ](f"tg://user?id={OWNER_ID}"))
 
 ★ /eval :- to evaluate simple code
 ★ /ex :-  to execute code
